38c3794b593c588c8b9fd323f712f2afb169bb52
Tyler <tyleralamo15@gmail.com>
2024-08-21 18:07:57 -0700

    Basic playerController stuff

--- Secret Scream Game Jam/Assets/Scripts/GameInput.cs
+++ Secret Scream Game Jam/Assets/Scripts/GameInput.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputActions playerInputActions;
+    private PlayerInputActions.PlayerActions player;
+    private PlayerController playerController;
+
+    void Awake()
     {
-        
+        playerInputActions = new PlayerInputActions();
+        player = playerInputActions.Player;
+        playerController = GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        playerController.OnMovement(player.Movement.ReadValue<Vector2>()); 
+    }
+
+    public void OnEnable()
+    {
+        player.Enable();
+    }
+
+    public void OnDisable()
+    {
+        player.Disable();
     }
 }
